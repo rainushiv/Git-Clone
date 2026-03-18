929e3283c50cc8d78954a674475a21a82ad3abbd
rainushiv <shivpreet567@gmail.com>
2026-03-18 14:35:54 -0400

    Feature: Added Helper functions to manipulate the Such repo_path, repo_file and repo_dir

--- libwyag.py
+++ libwyag.py
@@ -63,4 +63,26 @@ def repo_path(repo, *path):
     """Compute path under repo's gitdir"""
     return os.path.join(repo.gitdir, *path)
 
+def repo_file(repo,*path, mkdir = False):
+    """Same as repo path, but create dirname if absent"""
+    if repo_dir(repo, *path[:-1], mkdir=mkdir):
+        return repo_path(repo, *path)
+    
+def repo_dir(repo, *path, mkdir = False):
+
+    path = repo_path(repo, *path)
+    if os.path.exists(path):
+        if (os.path.isdir(path)):
+            return path
+        else:
+            raise Exception(f"Not a directory {path}")
+        
+    
+    if mkdir:
+        os.makedirs(path)
+        return path
+    else:
+        return None
+    
+
 
