b64944f00fef196e57fa3ed330618efd34c252e2
rainushiv <shivpreet567@gmail.com>
2026-03-18 14:05:50 -0400

    Feature: Added git Repository Object

--- libwyag.py
+++ libwyag.py
@@ -44,3 +44,23 @@ class GitRespository(object):
     def __init__(self,path, force = False):
         self.worktree = path
         self.gitdir = os.path.join(path, ".git")
+        if not (force or os.path.isdir(self.gitdir)):
+            raise Exception(f"Not a Git Repository {path}")
+        # Ingests config files found
+        self.conf = configparser.ConfigParser()
+        cf = repo_file(self,"config")
+        if cf and os.path.exists(cf):
+            self.conf.read([cf])
+        elif not force: 
+            raise Exception("Configuration file missing")
+
+        if not force:
+            vers = int(self.conf.get("core","respoitoryformatversion"))
+            if vers != 0:
+                raise Exception(f"Unsupported repositoryformatversion: {vers}")
+            
+def repo_path(repo, *path):
+    """Compute path under repo's gitdir"""
+    return os.path.join(repo.gitdir, *path)
+
+
