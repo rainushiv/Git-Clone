9ad64c5cf9cf056b020f4c8ffd6074e408a78613
rainushiv <shivpreet567@gmail.com>
2026-03-19 15:40:02 -0400

    Feat: Added function to create git Object as well as its path

--- libwyag.py
+++ libwyag.py
@@ -84,5 +84,38 @@ def repo_dir(repo, *path, mkdir = False):
     else:
         return None
     
+def repo_create(path):
+    """Creates New Respository at the path"""
+    repo = GitRespository(path, True)
+
+    #Make sure the path doesn't exist or is an 
+    #empty dir
+
+    if os.path.exists(repo.worktree):
+        if not os.path.isdir(repo.worktree):
+            raise Exception(f"{path} is not a directory!")
+        
+        if os.path.exists(repo.gitdir) and os.listdir(repo.gitdir):
+            raise Exception(f"{path} is not empty!")
+    
+    else:
+        os.makedirs(repo.worktree)
+
+    assert repo_dir(repo,"branches", mkdir = True)
+    assert repo_dir(repo,"objects", mkdir = True)
+    assert repo_dir(repo,"refs","tags", mkdir = True)
+    assert repo_dir(repo,"refs","heads", mkdir = True)
+
+    with open(repo_file(repo,"description"), "w") as f: 
+        f.write("Unamed respository; edit this file 'description' to name the respository.\n")
+
+    with open(repo_file(repo,"HEAD"),"w") as f:
+        f.write("ref: refs/heads/master\n")
+
+    with open(repo_file(repo,"config"),"w") as f:
+        config = repo_default_config()
+        config.write(f)
+
+    return repo 
 
 
