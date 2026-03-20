8c70eff67bfb2f9477e24214a6c18e962a59c995
rainushiv <shivpreet567@gmail.com>
2026-03-19 20:06:16 -0400

    Feature: Added Logic for creating INI File Stores formation such as: Repo format version File mode bare

--- libwyag.py
+++ libwyag.py
@@ -118,4 +118,13 @@ def repo_create(path):
 
     return repo 
 
+def repo_default_config():
+    ret = configparser.ConfigParser()
+    ret.add_section("core")
+    ret.set("core","repositoryformatversion","0")
+    ret.set("core","filemode","false")
+    ret.set("core","bare","false")
+    return ret
+
+
 
